f29927e5988d4857b9ad37f7c4739d7d2ab0bd86
João Victor Alves da Silva <joao.victor@roca-imoveis.local>
2021-03-28 18:12:38 -0300

    Hacker_chat

---
+++ client/index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+
+/*
+    chmod +x index.js
+*/
+//npm i -g @alvesjoaovictor/hacker-chat-client
+
+
+//npm unlink -g @alvesjoaovictor/hacker-chat-client
+/*
+hacker-chat \
+        --username joao_alves \
+        --room sala01 
+
+/*
+node ./index.js \
+        ----username joao_alves \
+        ----room sala01 \
+        ----hostUri localhost
+
+
+
+*/
+
+
+import Events from 'events'
+import CliConfig from './src/cliConfig.js';
+import EventManager from './src/eventManager.js';
+import SocketClient from './src/socket.js';
+import TerminalController from "./src/terminalController.js";
+
+
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+
+const componentEmitter = new Events()
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+const eventManager = new EventManager({ componentEmitter, socketClient})
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+
+const data = {
+    roomId: config.room,
+    userName: config.username
+}
+eventManager.joinRoomAndWaitForMessages(data)
+
+const controller = new TerminalController()
+await controller.initializeTable(componentEmitter)
+
